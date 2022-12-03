fcddb29b95cf14b43f7282884feeb2a165f37573
PLAYseung <yuga9812@gmail.com>
2022-12-03 14:05:23 +0900

    [2022.12.3] DI 테스트 케이스

--- infrealn_Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ infrealn_Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,16 +2,21 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
-//    회원 가입
+    // MemberRepository를 외부에서 넣어주도록
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    //    회원 가입
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원 x
         validateDuplicateMember(member); // 중복회원 검색
