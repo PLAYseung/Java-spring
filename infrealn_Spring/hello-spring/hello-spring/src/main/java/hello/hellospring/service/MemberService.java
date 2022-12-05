d86e68dbf7ad914f473a5370145e3fb34bc65b11
PLAYseung <yuga9812@gmail.com>
2022-12-05 19:57:23 +0900

    [2022.12.5] Spring - direct spring bean management
    - 생성자 주입
    - 필드 주입
    - setter 주입

--- infrealn_Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ infrealn_Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -8,13 +8,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // MemberRepository를 외부에서 넣어주도록
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
